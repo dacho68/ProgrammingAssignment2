51becb6de2562506b41d8f60fafad6444eb27d17
dacho68 <dacho68@gmail.com>
2016-04-12 10:37:51 -0400

    update

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ ## Initialize the inverse property
+  m<-NULL
+  
+  ## Method to set the matrix
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  ## Method the get the matrix
+  get <- function() {
+    ## Return the matrix
+    x
+  }
+  ## Method to set the inverse of the matrix
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  ## Method to get the inverse of the matrix
+  getInverse <- function() {
+    ## Return the inverse property
+    m
+  }
+  ## Return a list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
